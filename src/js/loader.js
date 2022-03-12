fc3421569fbc42f67b2d10be350745a4b709c191
nathannye <79227199+nathannye@users.noreply.github.com>
2022-03-11 22:04:41 -0500

    loader

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,4 +1,23 @@
+import gsap from "gsap";
+
 let container = document.getElementById("loader");
 let backer = document.getElementById("loaderBacker");
+import setupIntroTL from "./introAnims";
 
+let loader = gsap.timeline({
+  onComplete: setupIntroTL(),
+});
 
+loader.to(container, {
+  scale: 0.4,
+  duration: 1,
+});
+
+window.addEventListener("load", (event) => {
+  gsap.to(container, {
+    autoAlpha: 0,
+  });
+
+    setupIntroTL();
+    
+});
