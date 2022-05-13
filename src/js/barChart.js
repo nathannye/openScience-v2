be8a2c00249a6b882b23ee674561e4f0b7bd130c
nathannye <79227199+nathannye@users.noreply.github.com>
2022-05-13 14:51:03 -0400

    slight updates

--- src/js/barChart.js
+++ src/js/barChart.js
@@ -76,141 +76,151 @@ declarePandemic.setAttribute("class", "eventMarker");
 exascaleMark.setAttribute("id", "exascaleMark");
 exascaleMark.setAttribute("class", "eventMarker");
 
-function animateChart() {
-  let tillPandemic = bars.slice(0, 8);
-  let pandemicTillExa = bars.slice(9, bars.length);
-  let chartCaptions = document.querySelectorAll("#chartCaptionContainer > h2");
-  let introCaption = chartCaptions[0];
-  let pandemicCaption = chartCaptions[1];
-  let exaCaption = chartCaptions[2];
-  let chartSectionContainer = document.getElementById("chartOfUsers");
-  let barChartContainer = document.getElementById("barChartContainer");
+let tillPandemic = bars.slice(0, 8);
+let pandemicTillExa = bars.slice(9, bars.length);
+let chartCaptions = gsap.utils.toArray("#chartCaptionContainer > h2");
+let introCaption = chartCaptions[0];
+let pandemicCaption = chartCaptions[1];
+let exaCaption = chartCaptions[2];
+let chartSectionContainer = document.getElementById("chartOfUsers");
+let barChartContainer = document.getElementById("barChartContainer");
 
-  chartCaptions.forEach((caption) => {
-    caption.split = new SplitText(caption, {
-      type: "words, lines",
-      linesClass: "splitLine",
-    });
+// chartCaptions.forEach((caption) => {
+//   caption.split = new SplitText(caption, {
+//     type: "words, lines",
+//     linesClass: "splitLine",
+//   });
 
-    gsap.set(caption.split.words, {
-      yPercent: 100,
-    });
+//   gsap.set(caption.split.words, {
+//     yPercent: 100,
+//   });
 
-    caption.tl = gsap.timeline({
-      paused: true,
-    });
+//   caption.tl = gsap.timeline({
+//     paused: true,
+//   });
 
-    caption.tl.to(
-      caption.split.words,
-      {
-        yPercent: 0,
-        duration: 0.65,
-        ease: "power3.inOut",
-        stagger: 0.02,
-      },
-      "splitIn"
-    );
-  });
+//   caption.tl.to(
+//     caption.split.words,
+//     {
+//       yPercent: 0,
+//       duration: 0.65,
+//       ease: "power3.inOut",
+//       stagger: 0.02,
+//     },
+//     "splitIn"
+//   );
+// });
 
-  gsap.set(bars, {
+gsap.set(bars, {
+  opacity: 0,
+  fill: colors.teal,
+  scaleY: 0.35,
+  transformOrigin: "center bottom",
+});
+
+let graphtl = gsap.timeline({
+  scrollTrigger: {
+    pin: chartSectionContainer,
+    scrub: 0.35,
+    trigger: chartSectionContainer,
+    start: "bottom bottom",
+    end: "+=10000",
+  },
+});
+
+graphtl
+  // Call first caption
+  .from(chartCaptions[0], {
     opacity: 0,
-    fill: colors.teal,
-    scaleY: 0.35,
-    transformOrigin: "center bottom",
-  });
-
-  let graphtl = gsap.timeline({
-    paused: true,
-    scrollTrigger: {
-      pin: true,
-      scrub: 0.35,
-      trigger: chartSectionContainer,
-      start: "bottom bottom",
-    },
-  });
-
-  graphtl
-    .call(function () {
-      if (graphtl.reversed(true)) {
-        introCaption.tl.reverse();
-      } else {
-        introCaption.tl.play();
-      }
-    })
-    .to(tillPandemic, {
+    duration: 5,
+  })
+  // bars fill to pandemic bar
+  .to(
+    tillPandemic,
+    {
       opacity: 1,
       scaleY: 1,
-      delay: 80,
-      duration: 40,
       fill: colors.gry,
       stagger: {
         each: 12,
       },
-    })
-    .call(
-      function () {
-        if (graphtl.reversed(true)) {
-          pandemicCaption.tl.reverse();
-          introCaption.tl.play();
-        } else {
-          introCaption.tl.reverse();
-          pandemicCaption.tl.play();
-        }
+    },
+    "moveToPandemic"
+  )
+  // move over a bit
+  .to(
+    barChartContainer,
+    {
+      x: "-30vw",
+      ease: "power2.inOut",
+    },
+    "moveToPandemic"
+  )
+  // fill pandemic mark w/ yellow
+  .to(declarePandemic, {
+    opacity: 1,
+    scaleY: 1,
+    fill: colors.ylw,
+  })
+  // Next caption up, reverse the one before
+  .to(
+    chartCaptions[0],
+    {
+      opacity: 0,
+      duration: 5,
+    },
+    "swap1"
+  )
+  .from(
+    chartCaptions[1],
+    {
+      opacity: 0,
+      duration: 5,
+    },
+    "swap1"
+  )
+  .to(declarePandemic, {
+    duration: 1,
+  })
+  .to(
+    pandemicTillExa,
+    {
+      opacity: 1,
+      scaleY: 1,
+      duration: 1,
+      fill: colors.gry,
+      stagger: {
+        each: 0.2,
       },
-      null,
-      "pandemic"
-    )
-    .to(
-      declarePandemic,
-      {
-        opacity: 1,
-        delay: 7,
-        scaleY: 1,
-        fill: colors.ylw,
-        duration: 17,
-      },
-      "pandemic"
-    )
-    .to(
-      declarePandemic,
-      {
-        duration: 10,
-      },
-      "next"
-    )
-    .to(
-      pandemicTillExa,
-      {
-        opacity: 1,
-        scaleY: 1,
-        delay: 70,
-        duration: 65,
-        fill: colors.gry,
-        stagger: {
-          each: 5,
-        },
-      },
-      "next"
-    )
-    .call(function () {
-      if (graphtl.reversed(true)) {
-        exaCaption.tl.reverse();
-        pandemicCaption.tl.play();
-      } else {
-        pandemicCaption.tl.reverse();
-        exaCaption.tl.play();
-      }
-    })
-    .to(
-      barChartContainer,
-      {
-        x: "-100vw",
-        delay: 160,
-        ease: "power2.inOut",
-        duration: 200,
-      },
-      "next"
-    );
-}
-
-animateChart();
+    },
+    "moveTillExa"
+  )
+  .to(
+    barChartContainer,
+    {
+      x: "-100vw",
+    },
+    "moveTillExa"
+  )
+  .to(
+    chartCaptions[1],
+    {
+      opacity: 0,
+      duration: 5,
+    },
+    "swap1"
+  )
+  .from(
+    chartCaptions[2],
+    {
+      opacity: 0,
+      duration: 5,
+    },
+    "swap1"
+  );
+// .to(barChartContainer, {
+//   x: "-100vw",
+//   autoAlpha: 0,
+//   ease: "power2.inOut",
+//   duration: 20,
+// });
