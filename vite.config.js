5baecca69413a00bcec926bbaa160c25ef8ccbdd
nathannye <79227199+nathannye@users.noreply.github.com>
2022-09-07 20:06:11 -0400

    manifest + service worker with vite plugin pwa

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,37 @@
 import { defineConfig } from "vite";
-import pugPlugin from "vite-plugin-pug";
+import { VitePWA } from "vite-plugin-pwa";
 
-// export default defineConfig({
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//       "@classes": path.resolve(__dirname, "./src/classes"),
-//       "@sections": path.resolve(__dirname, "./src/sections"),
-//     },
-//   },
-//   plugins: [vue()],
-// });
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      includeAssets: [
+        "/icons/apple-touch-icon.png, /icons/favicon.png, robots.txt, ",
+      ],
+      manifest: {
+        name: "Open Source, Open Science",
+        short_name: "Open Science",
+        description: "Distributed computings incredible response to COVID-19.",
+        theme_color: "#030b18",
+        background_color: "#030b18",
+        icons: [
+          {
+            src: "/icons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
