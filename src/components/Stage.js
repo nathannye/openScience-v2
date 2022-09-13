4c7fcff844db8dc5afab52016366fdf665478338
Nathan Nye <79227199+nathannye@users.noreply.github.com>
2022-09-13 14:13:56 +0200

    reimport ngl

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,6 +1,6 @@
 import Component from "../classes/Component";
 import { colors } from "../data";
-// import * as NGL from "ngl";
+import * as NGL from "ngl";
 
 export default class Stage extends Component {
   constructor() {
